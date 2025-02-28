390aea15b0b68e77b6d5cff48440722bbb4a358a
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-02-28 20:41:53 +0530

    This is my first Python-based project. It includes a welcome page, login page, and registration page for staff.

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,26 @@
 from tkinter import *
+from tkinter import messagebox
+import subprocess
+
+def login_button():
+    root.destroy()
+    subprocess.run(['python', 'loginpage.py'])
+
+def registration_button():
+    root.destroy()
+    subprocess.run(['python', 'registrationpage.py'])
 
 root = Tk()
-root.title('Dashboard')
-root.geometry('400x250')
+root.title('Bank Management System - Dashboard')
+root.geometry('1920x1080')
 
-text = Label(root,
-             text = '''Welcome to Dashboard!, 
-             You are successfully registered and login.''')
-text.pack(padx = 5, pady = 50)
+headerpoint = Frame(root, bg = 'lightpink', height = 50)
+headerpoint.pack(fill = 'x')
+
+loginbutton = Button(headerpoint, text = 'Login', command = login_button)
+loginbutton.pack(side = 'right', padx = 5, pady = 5)
+
+registrationbutton = Button(headerpoint, text = 'Registration', command = registration_button)
+registrationbutton.pack(side = 'right', padx = 5, pady = 5)
 
 root.mainloop()
