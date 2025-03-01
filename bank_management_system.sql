1ef6b99a16c7c1864395fb615df4bc6b631d9b6e
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-01 13:41:53 +0530

    This is my first Python-based project. It includes a welcome page, login page, and registration page for staff.

--- bank_management_system.sql
+++ bank_management_system.sql
@@ -3,41 +3,41 @@ Create database Banking_Management_System;
 Use Banking_Management_System;
 
 create table staff_login(
-username varchar(50),
-password varchar (50)
+    username varchar(50),
+    password varchar (50)
 );
 
 create table staff_registeration(
-id int unique not null auto_increment,
-fullname varchar(50),
-username varchar(50),
-mobile_number bigint,
-age int,
-education_qualification varchar(50),
-job_type varchar(50),
-password varchar(50),
-confirm_password varchar(50)
+    id int unique not null auto_increment,
+    fullname varchar(50),
+    username varchar(50),
+    mobile_number bigint,
+    age int,
+    education_qualification varchar(50),
+    job_type varchar(50),
+    password varchar(50),
+    confirm_password varchar(50)
 );
 
 create table accounts(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(255) NOT NULL,
-balance DECIMAL(10,2) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    balance DECIMAL(10,2) NOT NULL
 );
 
 create table transactions(
-id INT AUTO_INCREMENT PRIMARY KEY,
-account_id INT,
-type ENUM('Deposit', 'Withdraw') NOT NULL,
-amount DECIMAL(10,2) NOT NULL,
-FOREIGN KEY (account_id) REFERENCES accounts(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT,
+    type ENUM('Deposit', 'Withdraw') NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
 create table loans(
-id INT AUTO_INCREMENT PRIMARY KEY,
-account_id INT,
-amount DECIMAL(10,2) NOT NULL,
-FOREIGN KEY (account_id) REFERENCES accounts(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT,
+    amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
 select * from staff_registeration;
