07f743b225c8b7b9b6b6ce263b7b3bc0a4d4fb50
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-01 12:11:32 +0530

    This is my first Python-based project. It includes a welcome page, login page, and registration page for staff.

--- dashboard.py
+++ dashboard.py
@@ -2,25 +2,41 @@ from tkinter import *
 from tkinter import messagebox
 import subprocess
 
-def login_button():
-    root.destroy()
-    subprocess.run(['python', 'loginpage.py'])
+def createaccount():
+    messagebox.showinfo(title = 'Account Created',
+                        message = ' Your Bank Account is successfully created.')
 
-def registration_button():
-    root.destroy()
-    subprocess.run(['python', 'registrationpage.py'])
+def depositmoney():
+    messagebox.showinfo(title = 'Money Deposited', message = 'Your Money is depositted.')
 
 root = Tk()
 root.title('Bank Management System - Dashboard')
 root.geometry('1920x1080')
 
-headerpoint = Frame(root, bg = 'lightpink', height = 50)
-headerpoint.pack(fill = 'x')
+headerpoint = Frame(root, bg = 'lightpink', width = 50)
+headerpoint.pack(fill = 'y', side = 'left')
 
-loginbutton = Button(headerpoint, text = 'Login', command = login_button)
-loginbutton.pack(side = 'right', padx = 5, pady = 5)
+create_account = Button(headerpoint,
+                        text = 'Create Account',
+                        bg = '#87CEFA',
+                        fg = 'white',
+                        font = ('Arial', 12, 'bold'),
+                        command = createaccount)
+create_account.pack(side = 'top',
+                    pady = 5,
+                    padx = 5)
 
-registrationbutton = Button(headerpoint, text = 'Registration', command = registration_button)
-registrationbutton.pack(side = 'right', padx = 5, pady = 5)
+deposit_money = Button(headerpoint,
+                       text =  'Deposit Money',
+                       bg = '#90EE90',
+                       fg = 'white',
+                       font = ('Arial', 12, 'bold'),
+                       command = depositmoney)
+deposit_money.pack(side = 'top',
+                   pady = 5,
+                   padx = 5)
+
+withdraw_money = Button(headerpoint, text = 'Withdraw Money', bg = '#90EE90', fg = 'White', font = ('Arial', 12, 'bold'))
+withdraw_money.pack(side = 'top', pady = 5, padx = 5)
 
 root.mainloop()
