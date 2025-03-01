d9e165d3e86768bc960deec99d7ecd54dbb45ebc
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-01 13:38:58 +0530

    This is my first Python-based project. It includes a welcome page, login page, and registration page for staff.

--- bank_management_system.sql
+++ bank_management_system.sql
@@ -19,6 +19,33 @@ password varchar(50),
 confirm_password varchar(50)
 );
 
+create table accounts(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+balance DECIMAL(10,2) NOT NULL
+);
+
+create table transactions(
+id INT AUTO_INCREMENT PRIMARY KEY,
+account_id INT,
+type ENUM('Deposit', 'Withdraw') NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
+FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+create table loans(
+id INT AUTO_INCREMENT PRIMARY KEY,
+account_id INT,
+amount DECIMAL(10,2) NOT NULL,
+FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
 select * from staff_registeration;
 
 select * from staff_login;
+
+select * from loans;
+
+select * from transactions;
+
+select * from accounts;
