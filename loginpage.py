ae9772d9d4151e5e85fa7b11255adc3142fbea74
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-04 12:10:47 +0530

    This is my first Python-based project. It includes a welcome page, login page, and registration page for staff.

--- loginpage.py
+++ loginpage.py
@@ -66,15 +66,17 @@ root.geometry('300x150')
 
 heading = Label(root,
                 text='Bank Management System',
-                font=('Arial', 12, 'bold'))
+                font=('Arial', 16, 'bold'))
 heading.grid(row=0,
              column=0,
              columnspan=2,
              pady=10)
 
 username_type = Label(root,
-                      text='Username')
-entry1 = Entry(root)
+                      text='Username',
+                      font = ('Arial', 12))
+entry1 = Entry(root,
+               font = ('Arial', 12))
 username_type.grid(row=1,
                    column=0,
                    padx=5,
@@ -86,9 +88,11 @@ entry1.grid(row=1,
             pady=2)
 
 password_type = Label(root,
-                      text='Password')
+                      text='Password',
+                      font = ('Arial', 12))
 entry2 = Entry(root,
-               show='*')
+               show='*',
+               font = ('Arial', 12))
 password_type.grid(row=2,
                    column=0,
                    padx=5,
@@ -101,14 +105,16 @@ entry2.grid(row=2,
 
 login_button = Button(root,
                       text='Login',
-                      command=loginbutton)
+                      command=loginbutton,
+                      font = ('Arial', 12))
 login_button.grid(row=3,
                   column=0,
                   pady=10)
 
 registration_button = Button(root,
                              text='For Registration',
-                             command=forregistration)
+                             command=forregistration,
+                             font = ('Arial', 12))
 registration_button.grid(row=3,
                          column=1,
                          pady=10)
